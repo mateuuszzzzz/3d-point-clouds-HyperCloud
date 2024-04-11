ca10fde6f9fd16bb04e7478d087fc42ee9df45b0
mateuuszzzzz <mateusz.rajski@student.uj.edu.pl>
2024-04-11 23:10:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,5 +21,16 @@ setup(
         'hypercloud.losses': 'hypercloud/losses',
         'hypercloud.datasets': 'hypercloud/datasets',
     },
+    install_requires={
+        'pyinn' @ 'git+https://github.com/mateuuszzzzz/pyinn/tree/cupy-fix@cupy-fix',
+        'plyfile==0.8.1',
+        'scikit-learn==0.22.1',
+        'scipy==1.7.3',
+        'torch==1.12.1',
+        'pandas==0.25.3',
+        'numpy==1.21.16',
+        'matplotlib==3.1.12',
+        'h5py==2.10.0',
+    },
     description='Auxiliary package to make HyperCloud code accessible from Gaussian Mesh Splatting (GaMeS).',
 )
